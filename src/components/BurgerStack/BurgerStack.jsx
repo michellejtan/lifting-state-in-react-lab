9c1cb17844d11f320fec80298c0a30822ee4483c
michellejtan <michelle0223@gmail.com>
2025-03-31 13:13:47 -0700

    renders burger stack & button to remove the ingredient from the stack

--- src/components/BurgerStack/BurgerStack.jsx
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -1,5 +1,22 @@
-const BurgerStack = (props) => {
-    return <ul>// map through props.ingredients</ul>;
+const BurgerStack = ({ stack, removeFromBurger }) => {
+    return (
+        <ul>
+            {stack.map((ingredient, index) => {
+                return (
+                    <li
+                        key={index}
+                        style={{ background: ingredient.color }}
+                    >
+                        {ingredient.name}
+                        <button onClick={() => removeFromBurger(index)} type="submit">X</button>
+
+                    </li>
+                );
+
+            })}
+
+        </ul>
+    );
 };
 
 export default BurgerStack;  
